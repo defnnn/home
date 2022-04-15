a84477de3ac90247ce19e30f3a316bfa29825996
Amanibhavam <iam@defn.sh>
2022-04-15 11:18:52 -0700

    run xbar commands under env.sh

--- Library/Application Support/xbar/plugins/defn.10m.sh
+++ Library/Application Support/xbar/plugins/defn.10m.sh
@@ -3,13 +3,13 @@
 function main {
     echo 'defn.dev'
     echo '---'
-    echo 'devcontainer | terminal=false | shell=/usr/local/bin/code | param1="--folder-uri" | param2="vscode-remote://dev-container+2f55736572732f6465666e2f776f726b2f646576/home/ubuntu"'
-    echo "vscode | terminal=false | shell=/usr/local/bin/code | param1='--folder-uri' | param2='code --folder-uri file://${HOME}/work'"
+    echo "devcontainer | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://dev-container+2f55736572732f6465666e2f776f726b2f646576/home/ubuntu'"
+    echo "vscode | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='code --folder-uri file://${HOME}/work'"
     for k3d in $(/usr/local/bin/k3d cluster list | awk 'NR > 1 {print $1}'); do
-        echo "vscode local ${k3d} | terminal=false | shell=/usr/local/bin/code | param1='--folder-uri' | param2='vscode-remote://k8s-container+context=k3d-${k3d}+namespace=${k3d}+podname=${k3d}+name=defn+image=remote+/home/ubuntu'"
+        echo "vscode local ${k3d} | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://k8s-container+context=k3d-${k3d}+namespace=${k3d}+podname=${k3d}+name=defn+image=remote+/home/ubuntu'"
     done
     for k3d in mini imac mbpro; do
-        echo "vscode remote ${k3d} | terminal=false | shell=/usr/local/bin/code | param1='--folder-uri' | param2='vscode-remote://k8s-container+context=k3d-${k3d}+namespace=${k3d}+podname=${k3d}+name=defn+image=remote+/home/ubuntu'"
+        echo "vscode remote ${k3d} | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://k8s-container+context=k3d-${k3d}+namespace=${k3d}+podname=${k3d}+name=defn+image=remote+/home/ubuntu'"
     done
 }
 
