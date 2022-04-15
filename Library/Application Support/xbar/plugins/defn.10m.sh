19425d33b63e1a3884a6d3031e4d5c21c13e10ea
Amanibhavam <iam@defn.sh>
2022-04-15 11:10:54 -0700

    devcontainer opens without terminal

--- Library/Application Support/xbar/plugins/defn.10m.sh
+++ Library/Application Support/xbar/plugins/defn.10m.sh
@@ -3,7 +3,7 @@
 function main {
     echo 'defn.dev'
     echo '---'
-    echo 'devcontainer | terminal=true | shell=/usr/local/bin/code | param1="--folder-uri" | param2="vscode-remote://dev-container+2f55736572732f6465666e2f776f726b2f646576/home/ubuntu"'
+    echo 'devcontainer | terminal=false | shell=/usr/local/bin/code | param1="--folder-uri" | param2="vscode-remote://dev-container+2f55736572732f6465666e2f776f726b2f646576/home/ubuntu"'
     echo "vscode | terminal=false | shell=/usr/local/bin/code | param1='--folder-uri' | param2='code --folder-uri file://${HOME}/work'"
     for k3d in $(/usr/local/bin/k3d cluster list | awk 'NR > 1 {print $1}'); do
         echo "vscode local ${k3d} | terminal=false | shell=/usr/local/bin/code | param1='--folder-uri' | param2='vscode-remote://k8s-container+context=k3d-${k3d}+namespace=${k3d}+podname=${k3d}+name=defn+image=remote+/home/ubuntu'"
