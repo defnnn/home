042af21c4d96defac86e17f97f9d692b62b0c48c
Amanibhavam <iam@defn.sh>
2022-04-15 12:35:01 -0700

    devcontainer

--- Library/Application Support/xbar/plugins/defn.10m.sh
+++ Library/Application Support/xbar/plugins/defn.10m.sh
@@ -3,13 +3,21 @@
 function main {
     cd
     export HOME="${HOME:-$(pwd)}"
+
+    local devcontainer="$(printf ${HOME} | od -A n -t x1 | tr -d '[\n\t ]')"
+
     echo 'defn.dev'
+
     echo '---'
-    echo "devcontainer | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://dev-container+2f55736572732f6465666e2f776f726b2f646576/home/ubuntu'"
+
+    echo "devcontainer | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://dev-container+${devcontainer}/home/ubuntu'"
+
     echo "vscode | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='code --folder-uri file://${HOME}/work'"
+
     for k3d in $(/usr/local/bin/k3d cluster list | awk 'NR > 1 {print $1}'); do
         echo "vscode local ${k3d} | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://k8s-container+context=k3d-${k3d}+namespace=${k3d}+podname=${k3d}+name=defn+image=remote+/home/ubuntu'"
     done
+
     for k3d in mini imac mbpro; do
         echo "vscode remote ${k3d} | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://k8s-container+context=k3d-${k3d}+namespace=${k3d}+podname=${k3d}+name=defn+image=remote+/home/ubuntu'"
     done
--- Makefile
+++ Makefile
@@ -3,8 +3,3 @@ up:
 
 down:
 	docker-compose down
-
-devcontainer-hex:
-	@echo -n code --folder-uri vscode-remote://dev-container+
-	@printf $(shell pwd)/work/dev | od -A n -t x1 | tr -d '[\n\t ]'
-	@echo /home/ubuntu
