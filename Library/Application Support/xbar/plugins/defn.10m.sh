26c908156313961e414c198b3fdf5d6e87b7435d
Amanibhavam <iam@defn.sh>
2022-04-15 09:30:38 -0700

    discover local k3d clusters

--- Library/Application Support/xbar/plugins/defn.10m.sh
+++ Library/Application Support/xbar/plugins/defn.10m.sh
@@ -4,9 +4,12 @@ function main {
     echo 'defn.dev'
     echo '---'
     echo 'devcontainer | terminal=true | shell=/usr/local/bin/code | param1="--folder-uri" | param2="vscode-remote://dev-container+2f55736572732f6465666e2f776f726b2f646576/home/ubuntu"'
-    echo 'vscode imac  | terminal=false | shell=/usr/local/bin/code | param1="--folder-uri" | param2="vscode-remote://k8s-container+context=k3d-imac+namespace=imac+podname=imac+name=defn+image=meh+/home/ubuntu"'
-    echo 'vscode mini  | terminal=false | shell=/usr/local/bin/code | param1="--folder-uri" | param2="vscode-remote://k8s-container+context=k3d-mini+namespace=mini+podname=mini+name=defn+image=meh+/home/ubuntu"'
-    echo 'vscode mbpro | terminal=false | shell=/usr/local/bin/code | param1="--folder-uri" | param2="vscode-remote://k8s-container+context=k3d-mbpro+namespace=mbpro+podname=mbprod+name=defn+image=meh+/home/ubuntu"'
+    for k3d in $(/usr/local/bin/k3d cluster list | awk 'NR > 1 {print $1}'); do
+        echo "vscode local ${k3d} | terminal=false | shell=/usr/local/bin/code | param1='--folder-uri' | param2='vscode-remote://k8s-container+context=k3d-${k3d}+namespace=${k3d}+podname=${k3d}+name=defn+image=remote+/home/ubuntu'"
+    done
+    for k3d in mini imac mbpro; do
+        echo "vscode remote ${k3d} | terminal=false | shell=/usr/local/bin/code | param1='--folder-uri' | param2='vscode-remote://k8s-container+context=k3d-${k3d}+namespace=${k3d}+podname=${k3d}+name=defn+image=remote+/home/ubuntu'"
+    done
 }
 
 main "$@"
