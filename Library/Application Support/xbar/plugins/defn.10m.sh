1b17f02fd9f13ff261a087c003332aa42d0891b2
Amanibhavam <iam@defn.sh>
2022-05-10 22:46:56 -0700

    add defn

--- Library/Application Support/xbar/plugins/defn.10m.sh
+++ Library/Application Support/xbar/plugins/defn.10m.sh
@@ -15,6 +15,7 @@ function main {
     echo "fly so   | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://ssh-remote+so/home/ubuntu'"
     echo "fly the  | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://ssh-remote+the/home/ubuntu'"
     echo "fly brie | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://ssh-remote+brie/home/ubuntu'"
+    echo "fly defn | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://ssh-remote+defn/home/ubuntu'"
 
     for k3d in $(/usr/local/bin/k3d cluster list | awk 'NR > 1 {print $1}'); do
         echo "vscode local ${k3d} | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://k8s-container+context=k3d-${k3d}+namespace=${k3d}+podname=${k3d}+name=defn+image=remote+/home/ubuntu'"
