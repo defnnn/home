5583a5bc8a08e25350418e160b4b3aa2e3686cd4
Amanibhavam <iam@defn.sh>
2022-06-12 13:12:34 -0700

    new devpod from menu

--- Library/Application Support/xbar/plugins/defn.10m.sh
+++ Library/Application Support/xbar/plugins/defn.10m.sh
@@ -4,23 +4,24 @@ function main {
     cd
     export HOME="${HOME:-$(pwd)}"
 
-    local devcontainer="$(printf ${HOME} | od -A n -t x1 | tr -d '[\n\t ]')"
-
-    echo 'defn.dev'
+    echo 'defn/dev'
 
     echo '---'
 
-    echo "macos | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='file://${HOME}/work'"
-    echo "devcontainer | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://dev-container+${devcontainer}/home/ubuntu'"
-    echo "devpod | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://k8s-container+context=k3d-k3s-default+namespace=default+podname=dev-0+name=dev+/home/ubuntu'"
-    echo "fly so   | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://ssh-remote+so/home/ubuntu'"
-    echo "fly the  | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://ssh-remote+the/home/ubuntu'"
-    echo "fly brie | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://ssh-remote+brie/home/ubuntu'"
-    echo "fly defn | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://ssh-remote+defn/home/ubuntu/work/cloud'"
+    #echo "macos | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='file://${HOME}/work'"
 
-    for k3d in $(/usr/local/bin/k3d cluster list | awk 'NR > 1 {print $1}'); do
-        echo "vscode local ${k3d} | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://k8s-container+context=k3d-${k3d}+namespace=${k3d}+podname=${k3d}+name=defn+image=remote+/home/ubuntu'"
-    done
+    #local devcontainer="$(printf ${HOME} | od -A n -t x1 | tr -d '[\n\t ]')"
+    #echo "devcontainer | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://dev-container+${devcontainer}/home/ubuntu'"
+
+    #echo "fly so   | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://ssh-remote+so/home/ubuntu'"
+    #echo "fly the  | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://ssh-remote+the/home/ubuntu'"
+    #echo "fly brie | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://ssh-remote+brie/home/ubuntu'"
+    #echo "fly defn | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://ssh-remote+defn/home/ubuntu/work/cloud'"
+
+    echo "devpod | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://k8s-container+context=k3d-k3s-default+namespace=default+podname=dev-0+name=dev+/home/ubuntu'"
+    echo "new devpod | terminal=false | shell=${HOME}/etc/env.sh | param1='bash' | param2='-c' | param3='cd ~/work/dev && make k3d attach'"
+
+    echo '---'
 
     for k3d in mini imac mbpro; do
         echo "vscode remote ${k3d} | terminal=false | shell=${HOME}/etc/env.sh | param1='code' | param2='--folder-uri' | param3='vscode-remote://k8s-container+context=k3d-${k3d}+namespace=${k3d}+podname=${k3d}+name=defn+image=remote+/home/ubuntu'"
