7a1c81722305096932109b98e930d844e8fdca65
Amanibhavam <iam@defn.sh>
2022-11-26 20:14:40 -0800

    yaegi suppports default systems

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     dev.url = github:defn/pkg?dir=dev&ref=dev-0.0.1;
-    defn-dev.url = github:defn/dev?ref=v0.0.19;
+    defn-dev.url = github:defn/dev?ref=v0.0.20;
   };
 
   outputs = inputs:
